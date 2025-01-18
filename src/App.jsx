29ff3e1ac46084181c7ada416b150641f7b99ac6
Roshan <88413854+Twinn-github09@users.noreply.github.com>
2025-01-18 23:09:54 +0530

    final front ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import Main from "./components/main/Main"
-import Sidebar from "./components/sidebar/Sidebar"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-const App = () => {
+import Navbar from "./components/Final/Navbar"; // Import Navbar
+import Home from "./components/Final/Home";
+import Main from "./components/Final/Main";
+import CaseStudies from "./components/Final/CaseStudies";
+import FAQ from "./components/Final/FAQ";
+import Gn1 from "./components/Final/Gn"
+import "./App.css";
+
+function App() {
   return (
-    <><Sidebar/>
-    <Main/>
-    </>
-  )
+    <Router>
+      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        {/* Navbar */}
+        <Navbar />
+        {/* Content Area */}
+        <div style={{ flex: 1, padding: "1rem", marginTop: "60px" }}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/case-studies" element={<CaseStudies />} />
+            <Route path="/Gn" element={< Gn1/>} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
